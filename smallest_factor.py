74bad0c39cccf24c61651f6a64ec5716739e155f
Gina F. Lamka <gfl0003@auburn.edu>
2021-03-03 12:08:26 -0500

    best practice 1
    I put the codes into functions. script currently works!

--- smallest_factor.py
+++ smallest_factor.py
@@ -4,12 +4,22 @@
 
 import sys
 
+###BEST PRACTICE
+def get_smallest_prime_factor(n):
+    for i in range(2, n):
+        if (n % i) == 0:
+            return i
+    return None
+
+
 if len(sys.argv) != 2:
     sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
 n = int(sys.argv[1])
 if n < 1:
     sys.exit(sys.argv[0] + ": Expecting a positive integer")
 
+##BEFORE
+"""
 smallest_prime_factor = None
 for i in range(2, n):
     if (n % i) == 0:
@@ -20,3 +30,12 @@ if smallest_prime_factor is None:
     print(n)
 else:
     print(smallest_prime_factor)
+"""
+smallest_prime_factor = get_smallest_prime_factor(n)
+
+if smallest_prime_factor is None:
+    print(n)
+else:
+    print(smallest_prime_factor)
+
+
